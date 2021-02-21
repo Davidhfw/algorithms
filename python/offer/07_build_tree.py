3590a89c865eee93f085d1d8fbcb9526baaaca09
Davidhfw <wuhaifenger@163.com>
2021-02-21 10:58:03 +0800

    Create 07_build_tree.py

---
+++ python/offer/07_build_tree.py
@@ -0,0 +1,18 @@
+'''
+输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+解题思路：使用前序遍历获取根节点，然后使用一个存储了中序遍历中值与下标对应的哈希表，找到根节点对应的下标，将序列划分为左右两个子树，递归重建左右子树
+'''
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        def recur(root, left, right):
+            if left > right:
+                return # 递归终止
+            node = TreeNode(preorder[root])
+            i = dic[preorder[root]]
+            node.left = recur(root + 1, left, i - 1)
+            node.right = recur(i - left + root + 1, i+1, right)
+            return node
+        dic, preorder = {}, preorder
+        for i in range(len(inorder)):
+            dic[inorder[i]] = i
+        return recur(0, 0, len(inorder) - 1)
